7dd9926cd2ceebea59f9a2482e15e86fcc5bf70e
Imanuel Yemini <52522490+imanuel300@users.noreply.github.com>
2025-09-21 13:20:43 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,15 +8,15 @@ sudo chown -R www-data:www-data /var/www/html/CornGetFromGit
 sudo chmod -R 775 /var/www/html/CornGetFromGit
 
 # יצירת קבצי מערכת
-sudo touch /var/www/html/CornGetFromGit/log.log
+#sudo touch /var/www/html/CornGetFromGit/update_process.log
 sudo touch /var/www/html/CornGetFromGit/last_commit.json
 sudo touch /var/www/html/CornGetFromGit/check_updates.lock
 
 # הגדרת הרשאות לקבצים
-sudo chown www-data:www-data /var/www/html/CornGetFromGit/log.log
+#sudo chown www-data:www-data /var/www/html/CornGetFromGit/update_process.log
 sudo chown www-data:www-data /var/www/html/CornGetFromGit/last_commit.json
 sudo chown www-data:www-data /var/www/html/CornGetFromGit/check_updates.lock
-sudo chmod 664 /var/www/html/CornGetFromGit/log.log
+sudo chmod 664 /var/www/html/CornGetFromGit/update_process.log
 sudo chmod 664 /var/www/html/CornGetFromGit/last_commit.json
 sudo chmod 664 /var/www/html/CornGetFromGit/check_updates.lock
 
@@ -25,10 +25,6 @@ sudo cp app.py /var/www/html/CornGetFromGit/
 sudo chown www-data:www-data /var/www/html/CornGetFromGit/app.py
 sudo chmod 755 /var/www/html/CornGetFromGit/app.py
 
-cp update_checker.conf /etc/nginx/sites-available/
-# יצירת קישור סימבולי לאפשר את האתר
-ln -sf /etc/nginx/sites-available/update_checker.conf /etc/nginx/sites-enabled/
-
 # העתקת והגדרת שירות המערכת
 sudo cp update_checker.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/update_checker.service
@@ -36,8 +32,7 @@ sudo chmod 644 /etc/systemd/system/update_checker.service
 # הפעלת השירות
 sudo systemctl daemon-reload
 sudo systemctl enable update_checker
-sudo systemctl restart update_checker
-sudo systemctl restart nginx
+sudo systemctl start update_checker
 
 # הוספת הרשאות sudo ספציפיות ל-www-data
 echo "www-data ALL=(ALL) NOPASSWD: /bin/chown -R www-data\:www-data /var/www/html/CornGetFromGit/*" | sudo tee -a /etc/sudoers.d/www-data-updates
