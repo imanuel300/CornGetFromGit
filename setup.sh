fef47f9500e89ec9526371cb521b07dd45ec9eb9
Imanuel Yemini <52522490+imanuel300@users.noreply.github.com>
2025-09-21 10:55:14 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # הפעלת השירות
+##sudo apt install gunicorn בגלל שלא רצים ב VENV
+
 #sudo nano  /etc/systemd/system/update_checker.service
 #sudo nano /etc/nginx/sites-available/update_checker.conf
 
@@ -8,4 +10,6 @@ sudo systemctl enable update_checker
 sudo systemctl restart update_checker
 sudo systemctl restart nginx
 
+sudo systemctl status update_checker
+sudo journalctl -u update_checker.service
 echo "ההתקנה הושלמה בהצלחה" 
